84d4340c97570ee723b8f40169f1140932ab4379
shashwot.niraula1990@gmail.com <niraulashashwot@gmail.com>
2024-02-22 15:27:23 -0800

    Added register and log in page

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 @app.route('/add_job')
 def addJob():
     return render_template('jobs.html')
